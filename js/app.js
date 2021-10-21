05a73e7944108ab2b9fe020281c021d5ee280da4
Jakob Hillebrand <74309148+jakobhillebrand@users.noreply.github.com>
2021-10-21 20:49:22 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ $(document).ready(function() {
     e.preventDefault();
     $('.js-off-canvas-toggle').toggleClass('is-active');
     $('.js-off-canvas-container').toggleClass('is-active');
+    $('.js-off-canvas-container').removeClass('is-active');
   });
 
 });
