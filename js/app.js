fb95fe78eb31cad1488bdf195fbd8c798fcb7e41
Jakob Hillebrand <74309148+jakobhillebrand@users.noreply.github.com>
2021-10-21 22:53:21 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -16,39 +16,23 @@ $(document).ready(function() {
 
   $('.js-off-canvas-toggle').click(function(e) {
     e.preventDefault();
+    e.stopPropagation();
     $('.js-off-canvas-toggle').toggleClass('is-active');
     $('.js-off-canvas-container').toggleClass('is-active');
   });
   
-  
-  
-  $('.js-off-canvas-container.is-active').click(function(e) {
-    e.preventDefault();
-    alert("Hello! I am an alert box BOTH!!");
-    $('.js-off-canvas-toggle').removeClass('is-active');
-    $('.js-off-canvas-container').removeClass('is-active');
-  });
-  
-  $('.is-active').click(function(e) {
-    e.preventDefault();
-    alert("Hello! I am an alert box -- is active!!");
-    $('.js-off-canvas-toggle').removeClass('is-active');
-    $('.js-off-canvas-container').removeClass('is-active');
-  });
-  
-  $('.js-off-canvas-container:after').click(function(e) {
-    e.preventDefault();
-    alert("Hello! I am an alert box -- :alert!!");
-    $('.js-off-canvas-toggle').removeClass('is-active');
-    $('.js-off-canvas-container').removeClass('is-active');
-  });
-  
-  
-  $(document).mouseup(function(e) {
-    var container = $('.c-off-canvas-content');
-    if (!container.is(e.target) && container.has(e.target).length === 0) {
-        alert("Hello! I am an alert -- mouse");
-    }
+
+$('.js-off-canvas-toggle').click(function(e){
+    e.stopPropagation();
 });
+  $('.js-off-canvas-container').click(function(e){
+    e.stopPropagation();
+});
+
+$('body,html').click(function(e){
+  $('.js-off-canvas-toggle').removeClass('is-active');
+    $('.js-off-canvas-container').removeClass('is-active');
+});
+  
 
 });
