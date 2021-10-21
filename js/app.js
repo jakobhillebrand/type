8ba6e9750577d45d89ca626405fbe2267f57a6a6
Jakob Hillebrand <74309148+jakobhillebrand@users.noreply.github.com>
2021-10-21 21:20:40 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
   
   $('.js-off-canvas-container::after').click(function(e) {
     e.preventDefault();
+    alert("Hello! I am an alert box!!");
     $('.js-off-canvas-toggle').toggleClass('is-active');
     $('.js-off-canvas-container').toggleClass('is-active');
   });
